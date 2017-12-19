a0ddcf8fd5eb349234479b1ce09935d5526eb2ff
Mikael Hernvall <mikael.hernvall@gmail.com>
2017-12-19 17:35:21 +0100

    Specify get_aiMesh method as const

--- header/Game/FAMesh.h
+++ header/Game/FAMesh.h
@@ -78,7 +78,7 @@ public:
 	// std::vector<glm::mat4> animatedXForm;
 	// std::vector<FABone *> bones;
 
-	SharedPtr<Mesh> get_aiMesh() { return _mesh; }; // PA2528-3 MEMORY FRAGMENTATION TEST CODE
+	SharedPtr<Mesh> get_aiMesh() const { return _mesh; }; // PA2528-3 MEMORY FRAGMENTATION TEST CODE
 
 private:
 	void load(aiMesh &mesh);
